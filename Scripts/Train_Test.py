ee2dbddebcdea0ba3b910f5b11f1df60f3c0c17d
Toluwani Aremu <45424924+Ti-Oluwanimi@users.noreply.github.com>
2022-09-01 11:43:36 +0400

    Updated the Train_Test split

--- Scripts/Train_Test.py
+++ Scripts/Train_Test.py
@@ -14,14 +14,14 @@ def train_test(model, train_loader, test_loader, n_epochs):
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     model = model.to(device=device)
     criterion=nn.CrossEntropyLoss()
-    optimizer=optim.Adam(model.parameters(),lr=1e-3)
-    scheduler = optim.lr_scheduler.StepLR(optimizer,step_size=20, gamma=0.1)
+    optimizer=optim.Adam(model.parameters(),lr=1e-5)
+    scheduler = optim.lr_scheduler.StepLR(optimizer,step_size=100, gamma=0.1)
     model.train()
     for epoch in range(1, n_epochs+1):
         train_accuracy = 0
         train_samples = 0
         train_loss = 0.0
-        for i, (data, targets) in tqdm(enumerate(train_loader)):
+        for batch_idx,(data, targets) in enumerate(tqdm(train_loader)):
             data = data.to(device=device)
             targets = targets.to(device=device)
             optimizer.zero_grad()
@@ -37,25 +37,22 @@ def train_test(model, train_loader, test_loader, n_epochs):
         # calculate average losses
         train_loss = train_loss / len(train_loader)
 
-        print('Epoch: {} \tTraining Loss: {:.6f}'.format(epoch, train_loss))
-        scheduler.step()
-
-    with torch.no_grad():
-        model.eval()
-        test_loss = 0
-        test_accuracy = 0
-        test_samples = 0
-        for data,targets in test_loader:
-            data = data.to(device=device)
-            targets = targets.to(device=device)
-            ## Forward Pass
-            scores = model(data)
-            loss = criterion(scores,targets)
-            predictions = torch.argmax(scores, dim=-1)
-            test_accuracy += (predictions == targets).sum()
-            test_samples += predictions.size(0)
-            test_loss += loss.item()
-        print(f"Test Accuracy: {(test_accuracy / test_samples)*100:.3f}")  
-    # model in evaluation mode
-    torch.save(model, 'Saved/' + str(model.__class__.__name__) + '.pt')
+        with torch.no_grad():
+            model.eval()
+            test_loss = 0
+            test_accuracy = 0
+            test_samples = 0
+            for batch_idx,(data, targets) in enumerate(tqdm(test_loader)):
+                data = data.to(device=device)
+                targets = targets.to(device=device)
+                ## Forward Pass
+                scores = model(data)
+                loss = criterion(scores,targets)
+                predictions = torch.argmax(scores, dim=-1)
+                test_accuracy += (predictions == targets).sum()
+                test_samples += predictions.size(0)
+                test_loss += loss.item() 
+            t_a = (test_accuracy / test_samples)*100
+        print('Epoch: {} \tTraining Loss: {:.6f}'.format(epoch, train_loss), f"  Test Accuracy: {t_a:.3f}")
+            
     return model
