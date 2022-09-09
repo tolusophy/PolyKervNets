ae849af37437bff3f744a4204c9eaae031016d86
Toluwani Aremu <45424924+Ti-Oluwanimi@users.noreply.github.com>
2022-09-09 16:32:37 +0400

    Add files via upload

--- Models/PKA8.py
+++ Models/PKA8.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 from torchvision import datasets
 import torchvision.transforms as transforms
 import numpy as np
-from ModelPKN import Kerv2d
+from Models.ModelPKN import Kerv2d
 from tqdm import tqdm
 import os
 import torch.nn.functional as F
@@ -12,21 +12,22 @@ import torch.optim as optim
 class PKA8(nn.Module):
     def __init__(self, img_channels=3, num_classes=10):
         super(PKA8, self).__init__()
-        self.kerv1 = Kerv2d(in_channels=img_channels, out_channels= 96, kernel_size= 3, stride=1, padding=0)
-        self.pool = nn.AvgPool2d(kernel_size=3, stride=2)
-        self.kerv2 = Kerv2d(in_channels=96, out_channels=256, kernel_size=3, stride= 1, padding= 1)
-        self.kerv3 = Kerv2d(in_channels=256, out_channels=384, kernel_size=3, stride= 1, padding= 1)
-        self.kerv4 = Kerv2d(in_channels=384, out_channels=384, kernel_size=3, stride=1, padding=1)
-        self.kerv5 = Kerv2d(in_channels=384, out_channels=256, kernel_size=3, stride=1, padding=1)
-        self.fc  = nn.Linear(in_features= 9216, out_features= num_classes)
+        self.kerv1 = Kerv2d(in_channels=img_channels, out_channels=64, kernel_size=3, stride=2, padding=1)
+        self.pool = nn.AvgPool2d(kernel_size=2)
+        self.kerv2 = Kerv2d(in_channels=64, out_channels=192, kernel_size=3, padding=1)
+        self.kerv3 = Kerv2d(in_channels=192, out_channels=384, kernel_size=3, padding=1)
+        self.kerv4 = Kerv2d(in_channels=384, out_channels=256, kernel_size=3, padding=1)
+        self.kerv5 = Kerv2d(in_channels=256, out_channels=256, kernel_size=3, padding=1)
+        self.fc  = nn.Linear(in_features=1024, out_features= num_classes)
+        self.drop = nn.Dropout(p=0.2, inplace=False)
 
     def forward(self, x):
-        x = self.kerv1(x)
-        x = self.kerv2(x)
+        x = self.drop(self.kerv1(x))
+        x = self.drop(self.kerv2(x))
         x = self.pool(x)
-        x = self.kerv3(x)
-        x = self.kerv4(x)
-        x = self.kerv5(x)
+        x = self.drop(self.kerv3(x))
+        x = self.drop(self.kerv4(x))
+        x = self.drop(self.kerv5(x))
         x = self.pool(x)
         x = x.reshape(x.shape[0], -1)
         x = self.fc(x)
